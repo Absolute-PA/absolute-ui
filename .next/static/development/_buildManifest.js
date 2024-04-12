a94f04630b382c1fb9c03a23edc5afcdf88784f6
Tien Bui <buiminhtien06@gmail.com>
2024-04-12 23:00:19 +1000

    Version: 0.1.19

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/text-to-audio":["static\u002Fchunks\u002Fpages\u002Ftext-to-audio.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftext-to-audio"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
