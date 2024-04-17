9696136c5826688154ba8f7678b54e3ef88879a3
Tien Bui <buiminhtien06@gmail.com>
2024-04-17 21:32:11 +1000

    Update script

--- scripts/startUI.sh
+++ scripts/startUI.sh
@@ -1,6 +1,6 @@
-# git checkout main
-# git fetch origin
-# git reset --hard origin/main
-# yarn 
-# cp .env.prod .env
+git checkout main
+git fetch origin
+git reset --hard origin/main
+yarn 
+cp .env.prod .env
 NODE_ENV=production NODE_OPTIONS='-r @newrelic/next' NEXT_PUBLIC_HTTPS=true node server.js
